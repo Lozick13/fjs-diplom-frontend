c58a135291860bd945d408cb1809aa80703aed1c
Lozick <dlozichnyy2005@gmail.com>
2025-07-09 15:25:21 +0700

    init

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.scss';
+import store from './redux/store/index.ts';
+import './reset.scss';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </StrictMode>,
+);
